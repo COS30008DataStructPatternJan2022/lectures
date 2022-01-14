00bf2e806ce36fb3e7d11f006002ec208dfb6f2a
Duc Minh Le <lemduc@gmail.com>
2022-01-14 07:11:47 +0700

    commit removal changes to repository

--- c++/primitivetypes/MainChars.cpp
+++
@@ -1,8 +0,0 @@
-#include "iostream"
-using namespace std;
-
-int main() {
-  unsigned char c = 'A';
-  
-  cout << "char: " << c << endl; 
-}
--- c++/primitivetypes/MainStrings.cpp
+++
@@ -1,7 +0,0 @@
-#include "iostream"
-using namespace std;
-
-int main() {
-  // multiline string literal
-  std::cout << "a really, really long string literal " "that spans two lines" << std::endl; 
-}
